22a1cafcb46b318d12d29b853195a1af1d118786
QiyueSheng <dereksheng1226@gmail.com>
2016-12-27 23:02:44 -0500

    add the static middleware so that express can host the Client directly.

--- Server/app.js
+++ Server/app.js
@@ -14,6 +14,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use( express.static(__dirname + "/../client") );
+
 var baseRouter = express.Router();
 
 
